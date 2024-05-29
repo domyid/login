a44d7c709876decb000da6e1ed3f34b3eca9e199
daulJsx <mrdaul09@gmail.com>
2024-05-29 18:12:56 +0700

    fixed: google sign In[1]

--- docs/index.js
+++ docs/index.js
@@ -1,34 +1,6 @@
-import qrcode from "https://cdn.skypack.dev/qrcode-generator-es6";
-
-
- function makeQrCode(text, size) {
-   const qrc = new qrcode(0, "H");
-   qrc.addData(text);
-   qrc.make();
-   let qr = qrc.createSvgTag({ cellSize: 8, margin: 4 });
-
-   // Create a wrapper div to set the size
-   const wrapper = document.createElement("div");
-   wrapper.innerHTML = qr;
-   const svg = wrapper.firstChild;
-   svg.setAttribute("width", size);
-   svg.setAttribute("height", size);
-
-   const qrCodeContainer = document.getElementById("qrCodeContainer");
-   qrCodeContainer.innerHTML = "";
-   qrCodeContainer.appendChild(wrapper);
- }
+import GenerateQrCode from "./scripts/qr-generator.js";
 
  document.addEventListener("DOMContentLoaded", function () {
-   makeQrCode("https://wa.me/6285314267742?text=missyouuu", 350);
+   GenerateQrCode("https://wa.me/6285314267742?text=missyouuu", 350);
  });
-
-
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
  
---
+++ docs/scripts/qr-generator.js
@@ -0,0 +1,21 @@
+import qrcode from "https://cdn.skypack.dev/qrcode-generator-es6";
+
+const GenerateQrCode = (text, size) => {
+  const qrc = new qrcode(0, "H");
+  qrc.addData(text);
+  qrc.make();
+  let qr = qrc.createSvgTag({ cellSize: 8, margin: 4 });
+
+  // Create a wrapper div to set the size
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = qr;
+  const svg = wrapper.firstChild;
+  svg.setAttribute("width", size);
+  svg.setAttribute("height", size);
+
+  const qrCodeContainer = document.getElementById("qrCodeContainer");
+  qrCodeContainer.innerHTML = "";
+  qrCodeContainer.appendChild(wrapper);
+};
+
+export default GenerateQrCode;
