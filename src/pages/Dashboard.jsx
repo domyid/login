a4bf083640d426581ea82bf884f573a923ed2e4d
daulJsx <mrdaul09@gmail.com>
2024-06-10 10:53:49 +0700

    feat: centering dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen">
       <h1>Dashboard</h1>
       <p>Welcome to the dashboard!</p>
     </div>
