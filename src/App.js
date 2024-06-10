390456ccc8f893f99d628d982793b2975c627362
daulJsx <mrdaul09@gmail.com>
2024-06-10 11:52:51 +0700

    fix: error import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import { AuthProvider } from "./utils/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 const App = () => {
   return (
