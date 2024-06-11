49560a4a5e04e8eae77f9dfef855edb2931fbab3
daulJsx <mrdaul09@gmail.com>
2024-06-11 12:32:42 +0700

    added: add state for user info

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -5,19 +5,22 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
-  const logIn = () => {
+  const logIn = (userInfo) => {
     setIsAuthenticated(true);
+    setUser(userInfo);
   };
 
   const logOut = () => {
     setIsAuthenticated(false);
+    setUser(null);
     navigate("/");
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, logIn, logOut }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, logIn, logOut }}>
       {children}
     </AuthContext.Provider>
   );
