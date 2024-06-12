1202f9b46f177e61d1e86ca1e54c5e5f4fc28ef9
daulJsx <mrdaul09@gmail.com>
2024-06-12 11:13:28 +0700

    fixed: error Uncaught SyntaxError: Cannot use import statement outside a module (at whatsauth.js:1:1)

--- src/components/WhatsAuthQr.jsx
+++ src/components/WhatsAuthQr.jsx
@@ -5,32 +5,38 @@ const WhatsAuthQR = () => {
   const { user } = useAuth();
 
   useEffect(() => {
-    const script1 = document.createElement("script");
-    script1.src = "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
-    script1.async = true;
-    document.body.appendChild(script1);
+    const loadScripts = async () => {
+      const script1 = document.createElement("script");
+      script1.src =
+        "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
+      script1.async = true;
+      document.body.appendChild(script1);
 
-    const script2 = document.createElement("script");
-    script2.src = "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
-    script2.async = true;
-    document.body.appendChild(script2);
+      const script2 = document.createElement("script");
+      script2.src = "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
+      script2.async = true;
+      document.body.appendChild(script2);
 
-    script1.onload = () => {
-      script2.onload = () => {
-        window.wauthparam.auth_ws =
-          "d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-        window.wauthparam.keyword =
-          "aHR0cHM6Ly93YS5tZS82Mjg5NTYwMTA2MDAwMD90ZXh0PXdoNHQ1YXV0aDA=";
-        window.wauthparam.tokencookiehourslifetime = 18;
-        window.wauthparam.redirect = "/auth";
-        window.deleteCookie(window.wauthparam.tokencookiename);
-        window.qrController(window.wauthparam);
+      script1.onload = () => {
+        script2.onload = () => {
+          window.wauthparam.auth_ws =
+            "d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
+          window.wauthparam.keyword =
+            "aHR0cHM6Ly93YS5tZS82Mjg5NTYwMTA2MDAwMD90ZXh0PXdoNHQ1YXV0aDA=";
+          window.wauthparam.tokencookiehourslifetime = 18;
+          window.wauthparam.redirect = "/auth";
+          window.deleteCookie(window.wauthparam.tokencookiename);
+          window.qrController(window.wauthparam);
+        };
       };
     };
 
+    loadScripts();
+
     return () => {
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
+      document.body
+        .querySelectorAll('script[src*="whatsauth"]')
+        .forEach((script) => script.remove());
     };
   }, []);
 
