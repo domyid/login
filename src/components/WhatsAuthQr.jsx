9ec2e08d02fcde71641e35a2a487840c7920c281
daulJsx <mrdaul09@gmail.com>
2024-06-14 04:06:29 +0700

    fixed: scan qr page UI components

--- src/components/WhatsAuthQr.jsx
+++ src/components/WhatsAuthQr.jsx
@@ -40,27 +40,31 @@ const WhatsAuthQR = () => {
   return (
     <div
       id="hasphonenumber"
-      className="w-full h-screen bg-blue-100 flex items-center justify-center"
+      className="w-full min-h-screen bg-blue-50 flex items-center justify-center"
     >
       <div className="w-96 bg-white rounded-xl p-6 shadow-lg">
         {user ? (
           <div className="text-center">
-            <h1 className="text-2xl font-bold mb-2">Welcome, {user.Name}</h1>
-            <p className="mb-4">
-              <strong>Email:</strong> {user.Email}
+            <h1 className="text-2xl font-bold mb-2">Welcome, {user.name}</h1>
+            <p className="mt-5 mb-5 badge badge-primary">
+              <strong>Email:</strong> {user.email}
             </p>
-            <img
-              src={user.picture}
-              alt={user.Name}
-              className="w-24 h-24 rounded-full mx-auto mb-4 shadow-2xl"
-            />
+
+            <div class="w-24 shadow-2xl rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img
+                src={user.picture}
+                alt={user.name}
+                className="w-24 h-24 rounded-full mx-auto mb-4 shadow-2xl"
+              />
+            </div>
+
             <p className="text-gray-700">
               Please scan the QR code with your WhatsApp camera to provide your
               phone number.
             </p>
           </div>
         ) : (
-          <p>Loading user information...</p>
+          <span className="loading loading-dots loading-lg"></span>
         )}
 
         <div className="flex justify-center mt-4 mb-4" id="whatsauthqr">
